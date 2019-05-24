fd97dc5baeb655ed6729aebe05612348bd03f93a
Chris Heydrick <cheydrick@gmail.com>
2019-05-24 14:46:22 -0500

    made alsobot class to better handle things

--- alsobot.py
+++ alsobot.py
@@ -5,24 +5,45 @@ import sys
 from matrix_bot_api.matrix_bot_api import MatrixBotAPI
 from matrix_bot_api.mregex_handler import MRegexHandler
 from matrix_bot_api.mcommand_handler import MCommandHandler
+from mcstatus import MinecraftServer
+
+class Alsobot(object):
+    def __init__(self, minecraftserver):
+        self.minecraftserver = minecraftserver
+        self.minecraftserver_handle = MinecraftServer(self.minecraftserver)
+
+    def default_response(self, room, event):
+        status = self.minecraftserver_handle.status()
+        if status.players.online == 0:
+            info_string = "The server has 0 players and replied in {1}ms"
+        else:
+            info_string = "The Server has {0} players and replied in {1}ms:\n".format(status.players.online, status.latency)
+            for p in range(status.players.online):
+                info_string += "{}\n".format(status.players.sample[p].name)
+        
+        room.send_text(info_string)
 
 def alsobot_test_callback(room, event):
-    room.send_text("Hi, " + event['sender'])
+    server = MinecraftServer(MINECRAFTSERVERNAME)
+    status = server.status()
+    info_string = "The server has {0} players and replied in {1} ms".format(status.players.online, status.latency)
+    room.send_text(info_string)
 
 if __name__ == '__main__':
     USERNAME = os.getenv('ALSOBOTUSER', "")
     PASSWORD = os.getenv('ALSOBOTPASSWORD', "")
     SERVER = os.getenv('ALSOBOTSERVER', "")
-
-    if (USERNAME or PASSWORD or SERVER) == "":
-        print("{} {} {}".format(USERNAME, PASSWORD, SERVER))
-        print("Set ALSOBOTUSER, ALSOBOTPASSWORD, and ALSOBOTSERVER environment variables!")
+    MINECRAFTSERVER = os.getenv('ALSOBOTMINECRAFTSERVER', "")
+    if (USERNAME or PASSWORD or SERVER or ALSOBOTMINECRAFTSERVER) == "":
+        #print("{} {} {}".format(USERNAME, PASSWORD, SERVER))
+        print("Set ALSOBOTUSER, ALSOBOTPASSWORD, ALSOBOTSERVER, or ALSOBOTMINECRAFTSERVER environment variables!")
         sys.exit()
 
-    alsobot = MatrixBotAPI(USERNAME, PASSWORD, SERVER)
-    alsobot_test_handler = MRegexHandler("alsobot", alsobot_test_callback)
-    alsobot.add_handler(alsobot_test_handler)
-    alsobot.start_polling()
+    matrix_bot = MatrixBotAPI(USERNAME, PASSWORD, SERVER)
+    alsobot = Alsobot("www.alsochris.com")
+    alsobot_test_handler = MRegexHandler("alsobot", alsobot.default_response)
+    matrix_bot.add_handler(alsobot_test_handler)
+    matrix_bot.start_polling()
 
     while True:
         input()
